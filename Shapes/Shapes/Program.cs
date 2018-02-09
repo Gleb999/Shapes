6f41e0c4e1b53fa3ebb245063caebb40900c7771
Gleb999 <lihachev99gleb@mail.ru>
2018-02-09 17:06:08 +0700

    abstract

---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Polymorphism *****\n");
+
+            Shape[] myShapes = { new Hexagon(), new Circle(), new Hexagon("Mick"),
+                new Circle("Beth"), new Hexagon("Linda")};
+
+            foreach (Shape s in myShapes)
+            {
+                s.Draw();
+            }
+            Console.ReadLine();
+        }
+    }
+
